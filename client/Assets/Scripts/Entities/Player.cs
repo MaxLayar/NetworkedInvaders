a7aac19a24366b8b539e06216679b417da7835b4
MaxLayar <maxarobaze@hotmail.fr>
2025-08-20 15:28:31 +0200

    Refactor Player Class
    + Switch to new Unity input system

--- client/Assets/Scripts/Entities/Player.cs
+++ client/Assets/Scripts/Entities/Player.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace NetworkedInvaders.Entity
 {
@@ -12,30 +14,34 @@ namespace NetworkedInvaders.Entity
         [SerializeField] private Transform bulletSpawnPoint;
         [SerializeField] private float bulletSpeed = 10f;
         [SerializeField] private float bulletCooldown = 0.5f;
+        [SerializeField] private float bulletLifeTime = 5f;
 
         private float lastBulletTime;
+        private Vector2 moveVector;
 
         private void Update()
         {
-            MovePlayer();
-            Shoot();
-        }
-
-        private void MovePlayer()
-        {
-            float moveInput = Input.GetAxis("Horizontal");
-            transform.Translate(Vector3.right * moveInput * speed * Time.deltaTime);
-        }
-
-        private void Shoot()
-        {
-            if (Input.GetKeyDown(KeyCode.Space) && Time.time > lastBulletTime + bulletCooldown)
+            if (moveVector != Vector2.zero)
             {
-                GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
-                Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
-                bulletRigidbody.velocity = Vector2.up * bulletSpeed;
-                lastBulletTime = Time.time;
+                transform.Translate(Vector3.right * moveVector * speed * Time.deltaTime);
             }
+        }
+
+        public void OnMove(InputValue iv)
+        {
+            moveVector = iv.Get<Vector2>();
+        }
+
+        public void OnShoot(InputValue iv)
+        {
+            if (Time.time <= lastBulletTime + bulletCooldown) 
+                return;
+            
+            GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = Vector2.up * bulletSpeed;
+            lastBulletTime = Time.time;
+
+            Destroy(bullet, bulletLifeTime);
         }
     }
 }
